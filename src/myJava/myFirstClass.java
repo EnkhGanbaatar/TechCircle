8da09af8e9611ccf374d9296ebb2e6deaed2fa32
Enkhmend Ganbaatar <mendee@Enkhmends-MacBook-Pro.local>
2020-01-29 20:30:18 -0500

    Mende's first commit

---
+++ src/myJava/myFirstClass.java
@@ -0,0 +1,81 @@
+package myJava;
+
+public class myFirstClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	String myFriend = "Pagma Battulga";	
+		
+		
+	System.out.println(myFriend);	
+		
+
+
+	String housePrice = "$650000";
+	
+	String address = "11713 Potomac Crossing Way";
+	
+	String techSchool = "TechCircle";
+	
+	System.out.println(techSchool + " is an It Bootcamp. And the Address is\n" + address + "." + " The medium price of the houses in the area is " + housePrice +".");
+	
+	
+	String firstName = "Enkhmend";
+	 
+	String lastName = "Ganbaatar";
+	
+	
+	System.out.println("My name is " +firstName + " and my lastName is " +lastName +" I'm so happy to be here in Techcircle " +"And Thank you for teaching us Java");
+	
+	
+	
+	
+	
+	
+		
+// below are end of class and main method
+		
+	int firstNumber = 20;
+	int secondNumber = 20;
+	
+	boolean a =(firstNumber==secondNumber);
+	
+	System.out.println(a);
+	
+	////////
+	Integer objectNumber = new Integer(6);
+	Integer objectSecNumber = new Integer(6);
+	
+	
+	boolean b = (objectNumber.equals(objectSecNumber));
+	
+	
+	
+	
+	System.out.println(b);
+	
+	////////////
+	
+	String myCar = "Toyota Prius V";
+	 
+	String price = "14k";
+	
+	String addressSec = "4078 Airline parkway";
+	
+	System.out.println("I bougth a car from " + addressSec + " The car's Model is " + myCar + " It cost me about " + price + "." );
+	
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+		
+	}
+
+}
