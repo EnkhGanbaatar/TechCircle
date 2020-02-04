f3b9f984b1685da6a33a9f4df2f764cfee00c519
Enkhmend Ganbaatar <mendee@Enkhmends-MacBook-Pro.local>
2020-02-04 18:37:17 -0500

    My Second Commit

---
+++ src/myJava/Home2.java
@@ -0,0 +1,127 @@
+package myJava;
+
+public class Home2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// Relational operators
+		
+		
+		
+		
+		long number1 = 23445;
+		
+		long number2 = 245345;
+		long number3 = 2343545;
+		long number4 = 2345;
+		long number5 = 23;
+
+		
+		
+		
+		boolean equals = number1 == number5;
+		boolean notEquals = number3 !=22;
+		
+		boolean greaterThan = number5>342324;
+		 boolean lessThan = number4 < number5;
+		 
+		
+		boolean ç = number1 >= number2;
+		 boolean lessThanOrEqualTo = number4 <= 2343245;
+		 
+		 System.out.println("equals Results are = " + equals);
+		 System.out.println("not Equals Results are = " + notEquals);
+		 System.out.println("greater Than Results are = " + greaterThan);
+		 
+		 System.out.println("less Than Results are = " + lessThan);
+		 
+		 System.out.println("less Than Or Equal To Results are = " + lessThanOrEqualTo);
+		
+		
+
+		 int aa=67;
+		 
+		 int a1 = 84;
+		
+		 int b1 = 73;
+		  
+		 int c1 = 32;
+		 
+		 int d1 = 16;
+		  
+		 boolean  booleanResults1 = ((a1==aa)&&(c1==d1));
+		
+		 boolean  booleanResults2 = ((a1==aa)||(c1==d1));
+		
+		 boolean  booleanResults3 = ((a1==aa)&&(c1==d1));
+		
+		
+		
+		
+
+			// assignment operation
+			 
+			 
+			 int a2 = 98;
+			 
+			 int b2 = 39;
+			 int c2 = 5;
+			 int d2 = 16;
+			 
+			 
+			 System.out.println(a2);
+			 
+				a2 = a2 + b2;
+
+			 
+			a2+=b2;
+			 
+			
+			
+			 System.out.println(a2);
+
+			 
+			 a2+=d2;
+			 
+			 System.out.println(a2);
+
+			 
+			 c2 = c2 - d2;
+			 
+			 
+			 System.out.println("C is" + c2);
+			
+			c2 -=d2;
+			
+			System.out.println("C is " + c2);
+			
+			d2*=a2;
+			
+			System.out.println("D is " + d2);
+			
+			
+			
+			d2/=b2;
+			System.out.println("D is " + d2);
+			
+			
+			d2%=a2;
+			
+			System.out.println("D is " + d2);
+			
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
--- src/myJava/myFirstClass.java
+++ src/myJava/myFirstClass.java
@@ -1,5 +1,6 @@
 package myJava;
 
+
 public class myFirstClass {
 
 	public static void main(String[] args) {
@@ -65,17 +66,145 @@ public class myFirstClass {
 	
 	System.out.println("I bougth a car from " + addressSec + " The car's Model is " + myCar + " It cost me about " + price + "." );
 	
+// 
+	// Relational operators
+	
+	
+	
+	
+	long number1 = 23445;
+	
+	long number2 = 245345;
+	long number3 = 2343545;
+	long number4 = 2345;
+	long number5 = 23;
 
 	
 	
 	
+	boolean equals = number1 == number5;
+	boolean notEquals = number3 !=22;
+	
+	boolean greaterThan = number5>342324;
+	 boolean lessThan = number4 < number5;
+	 
+	
+	boolean ç = number1 >= number2;
+	 boolean lessThanOrEqualTo = number4 <= 2343245;
+	 
+	 System.out.println("equals Results are = " + equals);
+	 System.out.println("not Equals Results are = " + notEquals);
+	 System.out.println("greater Than Results are = " + greaterThan);
+	 
+	 System.out.println("less Than Results are = " + lessThan);
+	 
+	 System.out.println("less Than Or Equal To Results are = " + lessThanOrEqualTo);
+	
+	 
+	 int aa=67;
+	 
+	 int a1 = 84;
+	
+	 int b1 = 73;
+	  
+	 int c1 = 32;
+	 
+	 int d1 = 16;
+	  
+	 boolean  booleanResults1 = ((a1==aa)&&(c1==d1));
+	
+	 boolean  booleanResults2 = ((a1==aa)||(c1==d1));
+	
+	 boolean  booleanResults3 = ((a1==aa)&&(c1==d1));
+	
+	// assignment operation
+	 
+	 
+	 int a2 = 98;
+	 
+	 int b2 = 39;
+	 int c2 = 5;
+	 int d2 = 16;
+	 
+	 
+	 System.out.println(a2);
+	 
+		a2 = a2 + b2;
+
+	 
+	a2+=b2;
+	 
+	
+	
+	 System.out.println(a2);
+
+	 
+	 a2+=d2;
+	 
+	 System.out.println(a2);
+
+	 
+	 c2 = c2 - d2;
+	 
+	 
+	 System.out.println("C is" + c2);
+	
+	c2 -=d2;
+	
+	System.out.println("C is " + c2);
+	
+	d2*=a2;
+	
+	System.out.println("D is " + d2);
+	
+	
+	
+	d2/=b2;
+	System.out.println("D is " + d2);
+	
+	
+	d2%=a2;
+	
+	System.out.println("D is " + d2);
+	
+	int a3, b3;
+	
+	a3 = 10;
+	b3 = (a3==1) ? 20 : 30;
+	
+	System.out.println("Value is b3 is : " + b3);
+	
+	
+	
+	b3 = (a3 == 10) ? 20 : 30;
+	
+	System.out.println("Value of b3 is : " + b3);
+	
+	
+	final int finalValue = 4;
+	
+	//    finalValue = a3;
 	
 	
 	
 	
 	
 	
-		
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	}
 
 }
